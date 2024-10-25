cf728547b0f5d7e4b3a82d3896780fa92e954d1a
dariogguillen <guillendario@gmail.com>
2024-10-25 17:30:57 -0600

    some changes

--- nvim/lua/config/plugins/colorscheme.lua
+++ nvim/lua/config/plugins/colorscheme.lua
@@ -1,30 +1,39 @@
 return {
-	-- {
-	-- 	-- "vague2k/vague.nvim",
-	-- 	-- "slugbyte/lackluster.nvim",
-	-- 	-- "AlexvZyl/nordic.nvim",
-	-- 	-- "gmr458/cold.nvim",
-	-- 	"HoNamDuong/hybrid.nvim",
-	-- 	lazy = false,
-	-- 	priority = 1000,
-	-- 	config = function()
-	-- 		vim.cmd([[colorscheme hybrid]])
-	-- 	end,
-	-- },
 	{
-		"EdenEast/nightfox.nvim",
-		priority = 1000, -- make sure to load this before all the other start plugins
+		-- 	-- "vague2k/vague.nvim",
+		-- 	-- "slugbyte/lackluster.nvim",
+		-- 	-- "AlexvZyl/nordic.nvim",
+		-- 	-- "gmr458/cold.nvim",
+		-- 	"HoNamDuong/hybrid.nvim",
+		"scottmckendry/cyberdream.nvim",
+		lazy = false,
+		priority = 1000,
 		config = function()
-			local options = {
-				dim_inactive = true,
-				-- transparent = true,
-			}
-
-			require("nightfox").setup({
-				options = options,
+			require("cyberdream").setup({
+				transparent = true,
+				italic_comments = true,
+				borderless_telescope = false,
+				theme = {
+					saturation = 0.8, -- accepts a value between 0 and 1. 0 = greyscale
+				},
 			})
-			-- load the colorscheme here
-			vim.cmd([[colorscheme nordfox]])
+			vim.cmd([[colorscheme cyberdream]])
 		end,
 	},
+	-- {
+	-- 	"EdenEast/nightfox.nvim",
+	-- 	priority = 1000, -- make sure to load this before all the other start plugins
+	-- 	config = function()
+	-- 		local options = {
+	-- 			dim_inactive = true,
+	-- 			-- transparent = true,
+	-- 		}
+	--
+	-- 		require("nightfox").setup({
+	-- 			options = options,
+	-- 		})
+	-- 		-- load the colorscheme here
+	-- 		vim.cmd([[colorscheme nordfox]])
+	-- 	end,
+	-- },
 }
